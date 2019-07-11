c3f92bcd759cc0c695321b9ee48c9fa6f65a0c96
violeta <violetikas@yahoo.com>
2019-07-11 16:14:27 +0300

    starting DB

---
+++ src/DB/DataBaseConnect.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace DB;
+use PDO;
+use PDOException;
+
+class DataBaseConnect
+{
+private $servername = "localhost";
+private $username = "username";
+private $password = "password";
+
+//public function connectToDB{
+//try {
+//$conn = new PDO("mysql:host=$servername", $username, $password);
+//    // set the PDO error mode to exception
+//$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//$sql = "CREATE DATABASE myDBPDO";
+//    // use exec() because no results are returned
+//$conn->exec($sql);
+//echo "Database created successfully<br>";
+//}
+//catch(PDOException $e)
+//    {
+//        echo $sql . "<br>" . $e->getMessage();
+//    }
+//
+//$conn = null;
+//
+//}
+
+
+}

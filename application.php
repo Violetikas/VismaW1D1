19963b65163830066c49ee6d7ddad6192e6ef66a
violetatamasauskiene <violetikas@yahoo.com>
2019-07-14 22:32:49 +0300

    started refactoring Main class

--- application.php
+++ application.php
@@ -3,3 +3,8 @@ require __DIR__ . '/vendor/autoload.php';
 
 $main = new Fikusas\Main();
 echo $main->mainApplication($argv)."\n";
+
+
+
+//cache ('cache', 86400)
+// $logger->info(sprintf("Completed in %.2f seconds", $timeKeeping->stopTime()));
--- src/Main.php
+++ src/Main.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 
 namespace Fikusas;
 
-
 use Fikusas\Cache;
 use Fikusas\FileRead\FileRead;
 use Fikusas\FileRead\FileReadFromInput;
@@ -14,39 +13,63 @@ use Fikusas\Hyphenation\SentenceHyphenator;
 use Fikusas\Log;
 use Fikusas\UserInteraction;
 use Fikusas\TimeKeeping\TimeKeeping;
+use Psr\SimpleCache\CacheInterface;
 
 class Main
 {
+
+
+    private $timeKeeping;
+    private $logger;
+    private $cache;
+    private $fileReader;
+    private $userInteraction;
+    private $hyphenate;
+    private $sentenceHyphenator;
+    private $fileReadFromInput;
+    private $optionDivider;
+
+
+    /**
+     * Main constructor.
+     * @param $timeKeeping
+     * @param $logger
+     * @param $cache
+     * @param $fileReader
+     * @param $userInteraction
+     * @param $hyphenate
+     * @param $sentenceHyphenator
+     * @param $fileReadFromInput
+     * @param $optionDivider
+     */
+    public function __construct(
+        TimeKeeping $timeKeeping,
+        Log\Logger $logger,
+        CacheInterface $cache,
+        FileRead $fileReader,
+        UserInteraction\UserInteraction $userInteraction,
+        WordHyphenator $hyphenate,
+        SentenceHyphenator $sentenceHyphenator,
+        FileReadFromInput $fileReadFromInput,
+        UserInteraction\OptionDivider $optionDivider
+    ) {
+        $this->timeKeeping = $timeKeeping;
+        $this->logger = $logger;
+        $this->cache = $cache;
+        $this->fileReader = $fileReader;
+        $this->userInteraction = $userInteraction;
+        $this->hyphenate = $hyphenate;
+        $this->sentenceHyphenator = $sentenceHyphenator;
+        $this->fileReadFromInput = $fileReadFromInput;
+        $this->optionDivider = $optionDivider;
+    }
+
+
     public function mainApplication($argv)
     {
-        $timeKeeping = new TimeKeeping();
+        $this->timeKeeping->startTime();
+        $this->fileReader->readHyphenationPatterns();
 
-        $timeKeeping->startTime();
-
-        $logger = new Log\Logger();
-
-        $cache = new Cache\FileCache('cache', 86400);
-
-        $fileReader = new FileRead($cache);
-
-        $syllables = $fileReader->readHyphenationPatterns();
-
-        $userInteraction = new UserInteraction\UserInteraction();
-
-        $userInput = $userInteraction->getUserInput($argv);
-
-        $hyphenate = new WordHyphenator($syllables, $cache);
-
-        $sentenceHyphenator = new SentenceHyphenator($logger, $hyphenate);
-
-        $fileReadFromInput = new FileReadFromInput();
-
-        $optionDivider = new UserInteraction\OptionDivider($hyphenate, $sentenceHyphenator, $fileReader,
-            $fileReadFromInput);
-
-        $result = $optionDivider->divideOptions($userInput);
-
-        $logger->info(sprintf("Completed in %.2f seconds", $timeKeeping->stopTime()));
 
         return $result;
     }

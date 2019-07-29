1da5a04339ec10215bee7a00786f1769261bf810
violeta <violetikas@yahoo.com>
2019-07-29 14:20:53 +0300

    Codeception testing started

---
+++ tests/api/APICest.php
@@ -0,0 +1,42 @@
+<?php 
+
+class APICest
+{
+    const FIND_IN_WORDS = 'mistranslate';
+
+    public function _before(ApiTester $I)
+    {
+    }
+
+    // tests
+    public function  getWordsViaAPI(ApiTester $I)
+    {
+        $I->wantToTest('Getting words');
+
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendGET('/words');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            'word' => self::FIND_IN_WORDS
+        ]);
+    }
+    public function insertWordViaAPI(ApiTester $I)
+    {
+        $I->wantToTest('Insertion of word');
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendPOST('/words/Gediminas');
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            'message' => 'Word updated'
+        ]);
+    }
+    public function tryDeleteWord(ApiTester $I)
+    {
+        $I->wantToTest('Deletion of word');
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendDELETE('/words/apple');
+        $I->seeResponseCodeIs(200);
+    }
+}
